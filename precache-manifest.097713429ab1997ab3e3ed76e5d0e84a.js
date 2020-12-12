cf2ef11a9daa32f885617614b1d9d3c160aa37c7
alexander-vera <jalexanderv.meneses@gmail.com>
2020-12-12 18:00:51 -0500

    Updates

---
+++ precache-manifest.097713429ab1997ab3e3ed76e5d0e84a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9a3be348fc2f48a8c1f6612ad241f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a5aaa7435d9f73a4b9a",
+    "url": "./static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "c1c39e94c964ed99733b",
+    "url": "./static/css/main.f80991e3.chunk.css"
+  },
+  {
+    "revision": "8a5aaa7435d9f73a4b9a",
+    "url": "./static/js/2.233e486e.chunk.js"
+  },
+  {
+    "revision": "b71a9a211f0db1aa60ef67bd9d8c361d",
+    "url": "./static/js/2.233e486e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c39e94c964ed99733b",
+    "url": "./static/js/main.12bfd564.chunk.js"
+  },
+  {
+    "revision": "acc926bb605940aaa5e6",
+    "url": "./static/js/runtime-main.cad7f980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
